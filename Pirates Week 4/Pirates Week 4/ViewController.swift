47e1ff221fce91782353a3f3410060e5192d2b91
JostieVb <joostvanbergen@live.nl>
2015-04-09 10:58:01 +0200

    asdf

--- Pirates Week 4/Pirates Week 4/ViewController.swift
+++ Pirates Week 4/Pirates Week 4/ViewController.swift
@@ -21,7 +21,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func parseJsonData(jsonData:AnyObject?)
+    {
+        //Create empty array for Pirates
+        var pirates = [Pirate]()
+        
+        var jsonConverted = JSON(jsonData!)
+        println(jsonConverted)
+        
+        for (index: String, subJson: JSON) in jsonConverted {
+            
+            let newPirate = Pirate()
+            newPirate.name = subJson["name"].string
+            newPirate.comments = subJson["comments"].string
+            newPirate.life = subJson["life"].string
+            pirates.append(newPirate)
+        }
+    }
 
-
+    
+    func loadJsonData()
+    {
+        var jSONrequest = Alamofire.request(.GET, "http://athena.fhict.nl/users/i886625/pirates.json")
+        jSONrequest.validate()
+        jSONrequest.responseJSON
+            { (urlREQ, urlResp, responsestring, error) -> Void in
+                
+                if error == nil
+                {
+                    println(responsestring)
+                }
+                else
+                {
+                    //Something went wrong
+                    println(error)
+                    
+                }
+        }
+    }
 }
-
