2d2ec6137974b2d604dbbd726bb3301de6e37eee
JostieVb <joostvanbergen@live.nl>
2015-04-23 11:17:56 +0200

    musifind

--- MusiFind/MusiFind/EditViewController.swift
+++ MusiFind/MusiFind/EditViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-@objc(Model)
 
 class EditViewController: UIViewController {
 
--- MusiFind/MusiFind/ProfileViewController.swift
+++ MusiFind/MusiFind/ProfileViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import CoreData
-@objc(Model)
 
 class ProfileViewController: UIViewController {
 
